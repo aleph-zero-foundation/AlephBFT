b74b5a2bfbd949eeaa44fbbc6a6d9c07aed02949
Michal Swietek <michal.swietek@cardinals.cc>
2021-01-26 13:02:47 +0100

    more complete skeleton

--- src/dag.rs
+++ src/dag.rs
@@ -1,41 +1,57 @@
-use std::{
-	collections::HashMap,
-	sync::Arc,
-};
-use crate::{
-	traits::Context,
-	nodes::NodeMap,
-};
-
+use crate::{nodes::NodeMap, traits::Environment};
+use std::{collections::HashMap, sync::Arc};
 
 #[derive(Clone)]
-pub(crate) struct Vertex<C: Context> {
-	parents: NodeMap<Option<Arc<Vertex<C>>>>,
-	hash: C::Hash
+pub(crate) struct Vertex<E: Environment> {
+	creator: usize,
+	parents: NodeMap<Option<Arc<Vertex<E>>>>,
+	hash: E::Hash,
 }
 
-
-pub(crate) struct Dag<C: Context> {
-	vertex_by_hash: HashMap<C::Hash,Arc<Vertex<C>>>,
+impl<E: Environment> Vertex<E> {
+	pub(crate) fn creator(&self) -> usize {
+		self.creator
+	}
+	pub(crate) fn hash(&self) -> E::Hash {
+		self.hash.clone()
+	}
 }
 
-impl<C: Context> Dag<C> {
-	pub(crate) fn new() -> Dag<C> {
+pub(crate) struct Dag<E: Environment> {
+	vertex_by_hash: HashMap<E::Hash, Arc<Vertex<E>>>,
+	post_insert: Vec<Box<dyn Fn(Arc<Vertex<E>>) + Send + Sync + 'static>>,
+}
+
+impl<E: Environment> Dag<E> {
+	pub(crate) fn new() -> Dag<E> {
 		Dag {
 			vertex_by_hash: HashMap::new(),
+			post_insert: vec![],
 		}
 	}
 
-	pub(crate) fn contains_hash(&self, hash: &C::Hash) -> bool {
+	pub(crate) fn contains_hash(&self, hash: &E::Hash) -> bool {
 		self.vertex_by_hash.contains_key(hash)
 	}
 
-	pub(crate) fn add_vertex(&mut self, hash: C::Hash, parents: NodeMap<Option<Arc<Vertex<C>>>>) {
+	pub(crate) fn add_vertex(
+		&mut self,
+		hash: E::Hash,
+		creator: usize,
+		parents: NodeMap<Option<Arc<Vertex<E>>>>,
+	) {
 		let vertex = Vertex {
 			parents,
 			hash,
+			creator,
 		};
 		self.vertex_by_hash.insert(vertex.hash, Arc::new(vertex));
 	}
 
+	pub(crate) fn register_post_insert_hook(
+		&mut self,
+		hook: Box<dyn Fn(Arc<Vertex<E>>) + Send + Sync + 'static>,
+	) {
+		self.post_insert.push(hook);
+	}
 }
--- src/traits.rs
+++ src/traits.rs
@@ -1,12 +1,10 @@
+use futures::{Sink, Stream};
 use std::{
 	fmt::{Debug, Display},
 	hash::Hash,
 };
 
-use crate::{
-	nodes::NodeMap,
-};
-
+use crate::skeleton::{Error, Message};
 
 //use datasize::DataSize;
 use serde::{de::DeserializeOwned, Serialize};
@@ -16,20 +14,38 @@ use serde::{de::DeserializeOwned, Serialize};
 pub trait NodeIdT: Clone + Display + Debug + Send + Eq + Hash + 'static {}
 impl<I> NodeIdT for I where I: Clone + Display + Debug + Send + Eq + Hash + 'static {}
 
-
 /// A hash, as an identifier for a block or unit.
 pub trait HashT:
-	Eq + Ord + Copy + Clone + Debug + Display + Hash + Serialize + DeserializeOwned
+	Eq
+	+ Ord
+	+ Copy
+	+ Clone
+	+ Default
+	+ Send
+	+ Sync
+	+ Debug
+	+ Display
+	+ Hash
+	+ Serialize
+	+ DeserializeOwned
 {
 }
 impl<H> HashT for H where
-	H: Eq + Ord + Copy + Clone + Debug + Display + Hash + Serialize + DeserializeOwned
+	H: Eq
+		+ Ord
+		+ Copy
+		+ Clone
+		+ Send
+		+ Sync
+		+ Default
+		+ Debug
+		+ Display
+		+ Hash
+		+ Serialize
+		+ DeserializeOwned
 {
 }
-
-
-
-pub trait Context: Clone + Debug + Eq + Ord + Hash {
+pub trait Environment {
 	/// Unique identifiers for nodes
 	type NodeId: NodeIdT;
 	/// Unique identifiers for units.
@@ -37,13 +53,18 @@ pub trait Context: Clone + Debug + Eq + Ord + Hash {
 	/// The ID of a consensus protocol instance.
 	type InstanceId: HashT;
 
-
+	type Crypto;
+	type In: Stream<Item = Message<Self::Hash>> + Send + Unpin;
+	type Out: Sink<Message<Self::Hash>, Error = Error> + Send + Unpin;
+	fn finalize_block(&self, _: Self::Hash);
+	fn best_block(&self) -> Self::Hash;
+	// sth needed in the future for randomness
+	fn crypto(&self) -> Self::Crypto;
+	fn consensus_data(&self) -> (Self::Out, Self::In);
 	fn hash(data: &[u8]) -> Self::Hash;
-
 	// fn verify_signature(
 	//     hash: &Self::Hash,
 	//     public_key: &Self::ValidatorId,
 	//     signature: &<Self::ValidatorSecret as ValidatorSecret>::Signature,
 	// ) -> bool;
 }
-
