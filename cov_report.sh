2a83d9c4bf88e4e3892f82e30b0aa4a9bc273f69
Michal Swietek <michal.swietek@cardinals.cc>
2021-06-11 14:12:30 +0200

    Add target name for coverage binary

--- cov_report.sh
+++ cov_report.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-version=$(cat /tmp/cov_bin_version)
-
 cargo cov -- show \
     --use-color \
     --ignore-filename-regex='/rustc' \
     --ignore-filename-regex='/.cargo/registry' \
     --instr-profile=aleph_bft.profdata \
-    --object target/debug/deps/aleph_bft-"$version" \
+    --object target/debug/deps/aleph_bft-coverage \
     --show-instantiations --show-line-counts-or-regions \
     --Xdemangler=rustfilt | less -R
--- gen_cov_data.sh
+++ gen_cov_data.sh
@@ -6,18 +6,16 @@ RUSTFLAGS="-Z instrument-coverage" \
     LLVM_PROFILE_FILE="aleph_bft-%m.profraw" \
     cargo +nightly test --tests $1 2> covtest.out
 
-cargo profdata -- merge -sparse aleph_bft-*.profraw -o aleph_bft.profdata
-
-rm aleph_bft-*.profraw
-
 version=$(grep Running covtest.out | sed -e "s/.*aleph_bft-\(.*\))/\1/")
 rm covtest.out
+cp target/debug/deps/aleph_bft-"$version" target/debug/deps/aleph_bft-coverage
 
-echo $version > /tmp/cov_bin_version
+cargo profdata -- merge -sparse aleph_bft-*.profraw -o aleph_bft.profdata
+rm aleph_bft-*.profraw
 
 cargo cov -- report \
     --use-color \
     --ignore-filename-regex='/rustc' \
     --ignore-filename-regex='/.cargo/registry' \
     --instr-profile=aleph_bft.profdata \
-    --object target/debug/deps/aleph_bft-"$version"
+    --object target/debug/deps/aleph_bft-coverage
