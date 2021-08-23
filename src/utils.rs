52c810cd711322172fc82ed7f8f1eb1be06a4855
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2021-08-23 13:16:48 +0200

    Simplified utils.rs:into_infinite_stream.

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,5 @@
-use futures::{future::ready, stream::iter, Future, FutureExt, Stream};
-use std::iter::repeat;
+use futures::{future::ready, stream::repeat, Future, FutureExt, Stream};
 
 pub(crate) fn into_infinite_stream<F: Future>(f: F) -> impl Stream<Item = ()> {
-    f.then(|_| ready(iter(repeat(())))).flatten_stream()
+    f.then(|_| ready(repeat(()))).flatten_stream()
 }
