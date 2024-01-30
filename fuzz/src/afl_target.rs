b6dd245aa620e44a9ec930976df64afb421a72e8
lesniak43 <lesniak43@gmail.com>
2024-01-30 13:29:03 +0100

    A0-1052: Remove fuzz temporarily (#403)

--- fuzz/src/afl_target.rs
+++
@@ -1,9 +0,0 @@
-use afl::fuzz;
-use aleph_bft_fuzz::{fuzz as fuzz_helper, FuzzNetworkData, ReadToNetworkDataIterator};
-
-fn main() {
-    fuzz!(|data: &[u8]| {
-        let data: Vec<FuzzNetworkData> = ReadToNetworkDataIterator::new(data).collect();
-        fuzz_helper(data, 4, None);
-    });
-}
--- fuzz/src/libfuzz_target.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-use aleph_bft_fuzz::{fuzz, FuzzNetworkData, ReadToNetworkDataIterator};
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &[u8]| {
-    let data: Vec<FuzzNetworkData> = ReadToNetworkDataIterator::new(data).collect();
-    fuzz(data, 4, None);
-});
